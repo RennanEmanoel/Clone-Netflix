b361046a61956224fcb053a0a6347089b8ebaeda
Ruan Trindade <rennanemanoel386@gmail.com>
2021-01-20 19:27:08 -0400

    Versão 2.0

---
+++ js/owl/main.js
@@ -0,0 +1,13 @@
+function info(){
+
+    Swal.fire({
+        title: 'Vikings',
+        color: '#fffff',
+        text: 'Ragnar Lothbrok, um jovem que se considera descendente de Odin,é um guerreiro, fazendeiro e chefe de família.', 
+        imageUrl: 'https://studiosol-a.akamaihd.net/uploadfile/letras/playlists/a/c/4/8/ac482bf3a585451da17d039600df441d.jpg',
+        background: '#030202',
+        imageWidth: 700,
+        imageHeight: 200,
+        imageAlt: 'Vikings',
+      })
+};
--- js/owl/setup.js
+++ js/owl/setup.js
@@ -4,13 +4,13 @@ $('.owl-carousel').owlCarousel({
     nav:true,
     responsive:{
         0:{
-            items:1
+            items:3
         },
         600:{
             items:3
         },
         1000:{
-            items:5
+            items:7
         }
     }
 })
